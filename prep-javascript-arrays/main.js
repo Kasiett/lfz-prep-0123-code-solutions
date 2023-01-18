95fe823cd934e0a8c3ae83a1969730644651fcaa
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-01-18 06:12:08 +0000

    Completed prep-javascript-arrays exersice

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,13 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+var firstElement = languages[0];
+console.log('The first Element of an Array is : ' + firstElement);
+
+var thirdElement = languages[2];
+console.log('The third Element of an Array is : ' + thirdElement);
+var length = languages.length;
+console.log('Array length is => ' + length);
+
+var lastElement = languages[length - 1];
+console.log('The last element of Array is => ' + lastElement);
